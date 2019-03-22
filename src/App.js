1c584ab93d7bd23469abc62e99310933f8473b76
Tony <solaris0403@gmail.com>
2019-03-22 21:39:06 +0800

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from "./component/Header/Header";
 import Main from "./component/Main/Main";
 import Footer from "./component/Footer/Footer";
+import 'antd/dist/antd.css';
 
 class App extends Component {
     render() {
--- src/component/Article/Article.js
+++ src/component/Article/Article.js
@@ -1,30 +1,71 @@
 import React, {Component} from 'react';
 import './Article.css';
 import ArticleItem from "../ListItem/ArticleItem";
+import {Pagination} from 'antd';
+
+
+const pageSize = 10;
+const initPage = 1;
 
 class Article extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            'posts': [],
+            'total': 0,
+            'totalPages': 0,
+        };
     }
 
     componentDidMount() {
-        let WPAPI = require('wpapi');
-        let wp = new WPAPI({endpoint: 'http://www.itenn.cc/index.php/wp-json'});
-        wp.posts().then((response) => {
-            this.setState(response);
-        });
+        this.fetchPosts(initPage);
     }
 
+    fetchPosts = (page) => {
+        let WPAPI = require('wpapi');
+        let wp = new WPAPI({endpoint: 'http://www.itenn.cc/index.php/wp-json'});
+        wp.posts()
+            .param('per_page', pageSize)
+            .param('page', page)
+            .then((response) => {
+                let posts = [];
+                Object.values(response).map((post) => {
+                    if (post.hasOwnProperty('id')) {
+                        posts.push(post);
+                    }
+                });
+                console.log('posts', posts);
+                let total = response['_paging']['total'];
+                let totalPages = response['_paging']['totalPages'];
+                this.setState({
+                    'posts': posts,
+                    'total': total,
+                    'totalPages': totalPages
+                });
+            });
+    };
+
+    onChange = (page) => {
+        this.fetchPosts(page);
+    };
+
     render() {
-        let content = Object.values(this.state).map((item) => {
-            if (item.hasOwnProperty('id')) {
-                return <ArticleItem post={item}/>;
-            }
+        let paginationStyle = {
+            visibility: this.state.total < pageSize ? 'hidden' : 'visible'
+        };
+        let posts = this.state.posts.map((post) => {
+            return <ArticleItem post={post}/>;
         });
         return (
             <div className="Article">
-                {content}
+                {posts}
+                <Pagination
+                    className='pagination'
+                    onChange={this.onChange}
+                    style={paginationStyle}
+                    total={this.state.total}
+                    defaultPageSize={pageSize}
+                    defaultCurrent={initPage}/>
             </div>
         );
     }
